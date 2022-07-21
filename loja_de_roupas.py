c29c5bb37d11e9a8047d0651b943a42235c883fa
MoisesHMQ <mendes.moisesmgs@gmail.com>
2022-07-21 15:12:06 -0300

    endpoints completos api pronta

--- loja_de_roupas.py
+++ loja_de_roupas.py
@@ -62,3 +62,66 @@ def camisetas():
     camisa.append(tipo)
     return jsonify(tipo)
 
+@app.route("/bermuda", methods=['POST'])
+def bermudas():
+    modelo = request.json
+    for modelo_gesso in bermuda:
+        if modelo_gesso["bermuda"] == modelo["bermuda"]:
+            return {"status": "Produto já cadastrado."}
+    modelo = {
+        "codigo_barras": str(uuid.uuid4()),
+        "bermuda": modelo["bermuda"],
+        "tamanho":modelo["tamanho"]
+        }
+    bermuda.append(modelo)
+    return jsonify(modelo)
+
+@app.route("/chinelo", methods=['POST'])
+def chinelos():
+    modelo = request.json
+    for modelo_gesso in chinelo:
+        if modelo_gesso["chinelo"] == modelo["chinelo"]:
+            return {"status": "Produto já cadastrado."}
+    modelo = {
+        "codigo_barras": str(uuid.uuid4()),
+        "chinelo": modelo["chinelo"],
+        "tamanho":modelo["tamanho"]
+        }
+    chinelo.append(modelo)
+    return jsonify(modelo)
+
+@app.route("/carrinho/compras")
+def carrinho():
+    return jsonify(camisa,bermuda,chinelo)
+
+
+@app.route("/excluir/camisa", methods=['POST'])
+def excluir_camisas():
+    itens = request.json
+    print(camisa)
+    for dell in camisa:
+        if dell["codigo_barras"] == itens["codigo_barras"]:
+            camisa.remove(dell)
+            return itens
+
+@app.route("/excluir/bermudas", methods=['POST'])
+def excluir_bermudas():
+    berma = request.json
+    print(bermuda)
+    for dell in bermuda:
+        if dell["codigo_barras"] == berma["codigo_barras"]:
+            bermuda.remove(dell)
+            return berma
+
+@app.route("/excluir/chinelo", methods=['POST'])
+def excluir_chinelo():
+    sandalia = request.json
+    print(chinelo)
+    for dell in chinelo:
+        if dell["codigo_barras"] == sandalia["codigo_barras"]:
+            chinelo.remove(dell)
+            return sandalia
+
+
+app.run()
+
